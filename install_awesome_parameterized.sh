c52f4754f9b0ddbc491060b3714264e44847bca6
Caleb <aquaductape@gmail.com>
2020-02-21 15:55:10 -0800

    install configuration for user root

--- install_awesome_parameterized.sh
+++ install_awesome_parameterized.sh
@@ -2,7 +2,6 @@
 set -e
 
 echo 'Installing Awesome Vim from '$1
-cd $1
 
 VIMRC="set runtimepath+=$1
 
@@ -17,25 +16,23 @@ catch
 endtry"
 
 if [ $2 == "--all" ]; then
+    echo "$VIMRC" > /root/.vimrc
+    echo "Installed the Ultimate Vim configuration for user root successfully! Enjoy :)))"
     USERS=($(ls -l /home | awk '{if(NR>1)print $9}'))
     for user in ${USERS[*]}; do
         homepath=$(eval echo "~$user")
-        IFS=''
-        echo $VIMRC > ${homepath}/.vimrc
-        unset IFS
-        echo "Installed the Ultimate Vim configuration for user $user successfully! Enjoy :-)"
+        echo "$VIMRC" > ${homepath}/.vimrc
+        echo "Installed the Ultimate Vim configuration for user $user successfully! Enjoy :)))"
     done
-    echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
+    echo "Installed the Ultimate Vim configuration successfully! Enjoy :)))"
     exit 0
 else
     SELECTED_USERS=(${@:2})
     echo "Selected users: ${SELECTED_USERS[@]}"
     for user in ${SELECTED_USERS[@]}; do
         homepath=$(eval echo "~$user")
-        IFS=''
-        echo $VIMRC > ${homepath}/.vimrc
-        unset IFS
-        echo "Installed the Ultimate Vim configuration for user $user successfully! Enjoy :-)"
+        echo "$VIMRC" > ${homepath}/.vimrc
+        echo "Installed the Ultimate Vim configuration for user $user successfully! Enjoy :)))"
     done
     exit 0
 fi
